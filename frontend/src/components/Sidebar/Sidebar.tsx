08633eff2f3e8c8ca7f5bcb87358fe4c9b06a7f5
TheMindlessMan <axelguimaraes2@gmail.com>
2023-11-10 18:39:22 +0000

    Added new settings page; added settings button to navbar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,33 @@
-type SidebarProps = {
-	//
-};
+import { Menu } from 'lucide-react';
 
-const Sidebar: React.FunctionComponent<
-	SidebarProps
-> = ({}): React.ReactNode => {
-	return <h1>Sidebar</h1>;
+import { Button } from '@/components/ui/button';
+
+import { type SidebarItemProps, sidebarItems } from './sidebar-items';
+import SidebarItem from './SidebarItem';
+
+const Sidebar: React.FunctionComponent = (): React.ReactNode => {
+	return (
+		<aside className='flex w-32 flex-col items-center justify-between space-y-12 border-r border-input p-8'>
+			{Object.keys(sidebarItems).map(
+				(categoty: string, index: number) => (
+					<div
+						key={`${categoty}-${index}`}
+						className='flex w-full flex-col items-center justify-between space-y-8'>
+						{sidebarItems[categoty]?.map(
+							(item: SidebarItemProps, index: number) => (
+								<SidebarItem
+									key={`${item.title}-${index}`}
+									href={item.href}
+									title={item.title}
+									icon={item.icon}
+								/>
+							),
+						)}
+					</div>
+				),
+			)}
+		</aside>
+	);
 };
 
 export default Sidebar;
