c702ab6d5fb3a7205a0a5c5d4aa34cb4dd211d03
insosso <74792561+insosso@users.noreply.github.com>
2023-11-13 20:30:20 +0000

    Create Sessions.cs
    feat: Add Sessions model
    
    Introduces the Sessions model to represent user sessions in the system. These details will be crucial for tracking session states and managing authentication in the system.

---
+++ backend/Models/Sessions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PulseConnect.Models
+{
+    [Table("Sessions")]
+    public class Sessions
+    {
+        [Key]
+        public string SessionID { get; set; }
+
+        [Required]
+        public string UserID { get; set; }
+
+        [ForeignKey("User")]
+        public virtual Users User { get; set; }
+
+        [Required]
+        public DateTime SessionStartTime { get; set; }
+        public DateTime? SessionEndTime; //This is not required, as the session may still be active
+
+        [Required]
+        public required string? SessionToken;
+    }
+}
