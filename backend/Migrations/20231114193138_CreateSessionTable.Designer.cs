847e95ddadf25e293ce5eb3faaeb4fd01e5f31ee
MarcioRibeiro20 <8200408@estg.ipp.pt>
2023-11-14 21:17:27 +0000

    Add Migration - CreateSessionTable
    Introduce migration for creating the Session table

--- backend/Migrations/20231114193138_CreateSessionTable.Designer.cs
+++ backend/Migrations/20231114193138_CreateSessionTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PulseConnect.Data;
 
@@ -11,9 +12,11 @@ using PulseConnect.Data;
 namespace PulseConnect.Migrations
 {
     [DbContext(typeof(APIDbContext))]
-    partial class APIDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231114193138_CreateSessionTable")]
+    partial class CreateSessionTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Migrations/20231114193138_CreateSessionTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PulseConnect.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSessionTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    SessionID = table.Column<string>(name: "Session ID", type: "nvarchar(450)", nullable: false),
+                    UserID = table.Column<string>(name: "User ID", type: "nvarchar(450)", nullable: false),
+                    SessionStartTime = table.Column<DateTime>(name: "Session Start Time", type: "datetime2", nullable: false),
+                    SessionEndTime = table.Column<DateTime>(name: "Session End Time", type: "datetime2", nullable: true),
+                    SessionToken = table.Column<string>(name: "Session Token", type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.SessionID);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Users_User ID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_User ID",
+                table: "Sessions",
+                column: "User ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sessions");
+        }
+    }
+}
