ebd9a9638539554027ace05ada956aa3deaedaec
MarcioRibeiro20 <8200408@estg.ipp.pt>
2023-11-14 21:20:40 +0000

    Update Program.cs
    Commented out line 42 in MigrationInitializer.
    The change is a temporary adjustment.

--- backend/Program.cs
+++ backend/Program.cs
@@ -39,7 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-DataBaseManagementService.MigrationInitialisation(app);
+//DataBaseManagementService.MigrationInitialisation(app);
 
 app.UseHttpsRedirection();
 
