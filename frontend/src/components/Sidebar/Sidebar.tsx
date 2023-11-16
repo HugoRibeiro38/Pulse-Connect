14872bbe95cc53b13fa215f8ecf34d714f0e2444
Sérgio Félix <sergio98@outlook.pt>
2023-11-16 18:31:33 +0000

    Update UI components and add search bar to Navbar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,31 +1,34 @@
-import { Menu } from 'lucide-react';
-
-import { Button } from '@/components/ui/button';
-
-import { type SidebarItemProps, sidebarItems } from './sidebar-items';
-import SidebarItem from './SidebarItem';
+import SidebarItem from '@/components/Sidebar/SidebarItem';
+import { type NavigationItemProps, navigationItems } from '@/data/navigation';
 
 const Sidebar: React.FunctionComponent = (): React.ReactNode => {
 	return (
-		<aside className='flex w-32 flex-col items-center justify-between space-y-12 border-r border-input p-8'>
-			{Object.keys(sidebarItems).map(
-				(categoty: string, index: number) => (
-					<div
-						key={`${categoty}-${index}`}
-						className='flex w-full flex-col items-center justify-between space-y-8'>
-						{sidebarItems[categoty]?.map(
-							(item: SidebarItemProps, index: number) => (
-								<SidebarItem
-									key={`${item.title}-${index}`}
-									href={item.href}
-									title={item.title}
-									icon={item.icon}
-								/>
-							),
-						)}
-					</div>
-				),
-			)}
+		<aside className='w-min border-r border-input p-8'>
+			<nav className='h-full w-full'>
+				<ul className='flex h-full w-full flex-col items-center justify-between'>
+					{Object.keys(navigationItems).map(
+						(categoty: string, index: number) => (
+							<li
+								key={`${categoty}-${index}`}
+								className='flex w-full flex-col items-center justify-between gap-y-8'>
+								{navigationItems[categoty]?.map(
+									(
+										item: NavigationItemProps,
+										index: number,
+									) => (
+										<SidebarItem
+											key={`${item.title}-${index}`}
+											href={item.href}
+											title={item.title}
+											icon={item.icon}
+										/>
+									),
+								)}
+							</li>
+						),
+					)}
+				</ul>
+			</nav>
 		</aside>
 	);
 };
