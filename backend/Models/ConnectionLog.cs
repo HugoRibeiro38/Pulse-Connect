77e4d96566f2621679c0f4da5749d3432a61f2ca
DANIEL TEIXEIRA <8200378@estg.ipp.pt>
2023-11-21 15:48:33 +0000

    complete model ConnectionLog

--- backend/Models/ConnectionLog.cs
+++ backend/Models/ConnectionLog.cs
@@ -1,6 +1,35 @@
-﻿namespace PulseConnect.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PulseConnect.Models
 {
+    [Table("ConnectionLog")]
     public class ConnectionLog
     {
+        /*Schema:
+         * - id_log : string [PK]
+         * - id_connection : string [FK]
+         * - action_date : datetime
+         * - description : string
+         */
+
+        [Key]
+        [Column("id_log")]
+        [Required]
+        public String ID_Log { get; set; }
+
+        [Column("id_connection")]
+        [Required]
+        //[ForeignKey("ID_Connection")]
+        public String ID_Connection { get; set; }
+
+        [Column("action_date")]
+        [Required]
+        public DateTime Action_Date { get; set; }
+
+        [Column("description")]
+        [Required]
+        public String Description { get; set; }
+
     }
 }
