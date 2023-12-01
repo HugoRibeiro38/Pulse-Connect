1ef59aea7c2fdf929f34c8855ebeb051bdee0d74
Sérgio Félix <sergio98@outlook.pt>
2023-12-01 04:43:00 +0000

    Delete searchParamsSchema and ISearchParams

--- frontend/src/validators/SearchParams.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const searchParamsSchema = z.object({
-	sort: z.enum(['name', 'time']).optional().default('name'),
-	order: z.enum(['asc', 'desc']).optional().default('asc'),
-});
-
-export type ISearchParams = z.infer<typeof searchParamsSchema>;
