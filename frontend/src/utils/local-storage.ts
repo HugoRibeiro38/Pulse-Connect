1a81a0570a09533db4b8038655e3550a29d32f89
Sérgio Félix <sergio98@outlook.pt>
2023-12-01 04:42:31 +0000

    Refactor code and update validators

--- frontend/src/utils/local-storage.ts
+++ frontend/src/utils/local-storage.ts
@@ -10,21 +10,26 @@ export const writeOnLocalStorage = <T>({
 }): void => {
 	if (!isLocalStorageSupported())
 		throw new Error('Local Storage is not supported!');
+	
 	localStorage.setItem(key, JSON.stringify(data));
 };
 
 export const readFromLocalStorage = <T>({ key }: { key: string }): T | null => {
 	if (!isLocalStorageSupported())
 		throw new Error('Local Storage is not supported!');
+
 	const storedData = localStorage.getItem(key);
+
 	if (storedData) {
 		return JSON.parse(storedData) as T;
 	}
+
 	return null;
 };
 
 export const removeFromLocalStorage = ({ key }: { key: string }): void => {
 	if (!isLocalStorageSupported())
 		throw new Error('Local Storage is not supported!');
+
 	localStorage.removeItem(key);
 };
--- frontend/src/validators/User.ts
+++ frontend/src/validators/User.ts
@@ -1,24 +1,23 @@
 import { z } from 'zod';
 
-const minLengthErrorMessage = (type: string, number: number) =>
-	`${type} must be at least ${number} characters long.`;
-const maxLengthErrorMessage = (type: string, number: number) =>
-	`${type} cannot be longer than ${number} characters.`;
+import {
+	firstNameConstraints,
+	lastNameConstraints,
+	maxLengthErrorMessage,
+	minLengthErrorMessage,
+} from './general';
 
 export const UserProfileInformationSchema = z.object({
-	firstName: z
-		.string()
-		.min(3, { message: minLengthErrorMessage('First name', 3) })
-		.max(32, { message: maxLengthErrorMessage('First name', 32) }),
-	lastName: z
-		.string()
-		.min(3, { message: minLengthErrorMessage('Last name', 3) })
-		.max(32, { message: maxLengthErrorMessage('Last name', 32) }),
+	firstName: firstNameConstraints(),
+	lastName: lastNameConstraints(),
 	bio: z
 		.string()
 		.optional()
 		.or(
-			z.string().max(144, { message: maxLengthErrorMessage('Bio', 144) }),
+			z
+				.string()
+				.trim()
+				.max(144, { message: maxLengthErrorMessage('Bio', 144) }),
 		),
 	country: z.string().optional(),
 	city: z
@@ -27,10 +26,11 @@ export const UserProfileInformationSchema = z.object({
 		.or(
 			z
 				.string()
+				.trim()
 				.min(3, { message: minLengthErrorMessage('City', 3) })
 				.max(64, { message: maxLengthErrorMessage('City', 64) }),
 		),
-	url: z.string().optional().or(z.string().url()),
+	customURL: z.string().optional().or(z.string().trim().url()),
 });
 
 export type UserProfileInformation = z.infer<
