c9b8c9c44b9697e9c2ff57fe3139f14980e24c54
Sérgio Félix <sergio98@outlook.pt>
2023-12-01 04:44:40 +0000

    Add MobileSidebar component and link to testing page

--- frontend/src/app/testing/page.tsx
+++ frontend/src/app/testing/page.tsx
@@ -1,11 +1,5 @@
 import { Fragment } from 'react';
 
-import AddConnectionButton from '@/components/Testing/AddConnectionButton';
-import EditProfileButton from '@/components/Testing/EditProfileButton';
-import MessageButton from '@/components/Testing/MessageButton';
-import MoreOptionsButton from '@/components/Testing/MoreOptionsButton';
-import RemoveConnectionButton from '@/components/Testing/RemoveConnectionButton';
-
 type TestingProps = {
 	//
 };
@@ -15,13 +9,8 @@ const Testing: React.FunctionComponent<
 > = ({}): React.ReactNode => {
 	return (
 		<Fragment>
-			<div className='container mx-auto flex h-screen flex-row items-center justify-center gap-4 px-4 sm:px-6 lg:px-8'>
-				<AddConnectionButton userId={'1'} />
-				<RemoveConnectionButton userId={'1'} />
-				<EditProfileButton />
-				<MoreOptionsButton />
-				<MessageButton userId={'1'} />
-			</div>
+			<h1>Testing</h1>
+			<p>Testing page</p>
 		</Fragment>
 	);
 };
