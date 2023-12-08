39e6f8a6be921648c7e3db37bd3b477104d3b64e
DANIEL TEIXEIRA <8200378@estg.ipp.pt>
2023-12-08 22:15:59 +0000

    model of ThirdPartyAccount

---
+++ backend/Models/ThirdPartyAccount.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PulseConnect.Models
+{
+    [Table("ThirdPartyAccount")]
+    public class ThirdPartyAccount
+    {
+        [Key, Column("id"), Required]
+        public int ThirdPartyAccountId { get; set; }
+
+        [Column("UserId"), Required]
+        public string UserId { get; set; }
+
+        [Column("AccountId"), Required]
+        public string AccountType { get; set; }
+    }
+}
