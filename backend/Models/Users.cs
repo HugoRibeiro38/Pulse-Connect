9d101f7a5fcc58b6530ab06e89efb50e38d40b13
Hugo <ribeirohugo9400@gmail.com>
2023-12-12 20:58:40 +0000

    resolve conflict

--- backend/Models/Users.cs
+++ backend/Models/Users.cs
@@ -9,13 +9,28 @@ namespace PulseConnect.Models
     [Table("Users")]
     public class Users : IdentityUser
     {
+
+        // 
+
         [Key]
         [Column("id")]
         [Required]
         public String ID { get; set; }
+<<<<<<< Updated upstream
 
         [Column("UserName")]
         [Required(ErrorMessage = "O campo username é obrigatório")]
+=======
+        
+
+
+#pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
+
+        [Column("UserName")]
+#pragma warning disable CS0114 // O membro oculta o membro herdado; palavra-chave substituta ausente
+#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
+
+>>>>>>> Stashed changes
         public String UserName { get; set; }
 
         [Column("UserEmail")]
@@ -63,6 +78,12 @@ namespace PulseConnect.Models
 
         [Column("Multi_FactorType")]
         public DefaultType Multi_FactorType { get; set; }
+
+        [Column("IsOnline")]
+        public bool IsOnline { get; set; } = false;
+
+        [Column("IsAccountActive")]
+        public bool IsAccountActive { get; set; } = true;
 
     }
 
